de236335fda574a8d74c6201876eddf899289de7
valdinarvp <valdinar-pereira@hotmail.com>
2021-05-06 22:48:55 -0300

    Diferença entre Merge Persist

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -11,6 +11,45 @@ import com.algaworks.ecommerce.model.Produto;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 	
 	@Test
+    public void mostrarDifencaPersistMerge() {
+        Produto produtoPersist = new Produto();
+
+        produtoPersist.setId(5);
+        produtoPersist.setNome("Smartphone One Plus");
+        produtoPersist.setDescricao("O processador mais rápido.");
+        produtoPersist.setPreco(new BigDecimal(2000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produtoPersist);
+        produtoPersist.setNome("Smartphone Two Plus");
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacaoPersist = entityManager.find(Produto.class, produtoPersist.getId());
+        Assert.assertNotNull(produtoVerificacaoPersist);
+
+
+
+        Produto produtoMerge = new Produto();
+
+        produtoMerge.setId(6);
+        produtoMerge.setNome("Notebook Dell");
+        produtoMerge.setDescricao("O melhor da categoria.");
+        produtoMerge.setPreco(new BigDecimal(2000));
+
+        entityManager.getTransaction().begin();
+		produtoMerge = entityManager.merge(produtoMerge);
+        produtoMerge.setNome("Notebook Dell 2");
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacaoMerge = entityManager.find(Produto.class, produtoMerge.getId());
+        Assert.assertNotNull(produtoVerificacaoMerge);
+    }
+	
+	@Test
 	public void inserirObjetoComMerge() {
 
 		Produto produto = new Produto();
@@ -36,11 +75,9 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
 		Produto produto = entityManager.find(Produto.class, 1);
 
 		entityManager.getTransaction().begin();
-		
 		produto.setNome("Kindle Paperwhite 2ª geração");
-		
 		entityManager.getTransaction().commit();
-
+		
 		entityManager.clear();
 
 		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
